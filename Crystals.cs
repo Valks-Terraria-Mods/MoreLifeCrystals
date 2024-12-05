994ca8358a2a7a1a3cb3477f256f052af00d3e19
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-12-04 21:23:07 -0600

    1.4.3

--- Crystals.cs
+++ Crystals.cs
@@ -1,28 +1,42 @@
-﻿using Terraria.WorldBuilding;
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
 namespace MoreLifeCrystals;
+
+public class Config : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ClientSide;
+
+    [DefaultValue(100)]
+    public int LifeCrystalsAmount;
+}
 
 public class Crystals : ModSystem
 {
     public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
     {
         int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Life Crystals"));
+
         if (ShiniesIndex != -1)
+        {
             tasks.Insert(ShiniesIndex + 1, new PassLegacy(nameof(MoreLifeCrystals), LifeCrystals));
+        }
     }
 
     private void LifeCrystals(GenerationProgress progress, GameConfiguration configuration)
     {
         progress.Message = "Adding even more life crystals";
 
-        var amount = 100;
+        int amount = ModContent.GetInstance<Config>().LifeCrystalsAmount;
 
         for (int n = 0; n < amount; n++)
         {
-            for (var k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++)
+            for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++)
             {
-                var x = WorldGen.genRand.Next(50, Main.maxTilesX - 50);
-                var y = WorldGen.genRand.Next((int)GenVars.rockLayerLow, Main.maxTilesY);
-                WorldGen.AddLifeCrystal(x, y);
+                int x = WorldGen.genRand.Next(50, Main.maxTilesX - 50);
+                int y = WorldGen.genRand.Next((int)GenVars.rockLayerLow, Main.maxTilesY);
+
+                _ = WorldGen.AddLifeCrystal(x, y);
             }
         }
     }
